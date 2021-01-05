fe018437bfada6e861511c47f47c2f9593882e62
ImSoZRious <pannawich.tata@gmail.com>
2021-01-05 12:25:49 +0700

    add auto default saving with issue

--- commands/setup.js
+++ commands/setup.js
@@ -1,4 +1,6 @@
 const notify = require('./helper/notify.js');
+const fs = require('fs');
+const { config } = require('process');
 
 module.exports = {
 	name: 'setup',
@@ -7,7 +9,14 @@ module.exports = {
 	execute(msg) {
         if (notify.getChannel() !== msg.channel) {
             notify.setChannel(msg.guild.channels.cache.get(msg.channel.id));
-            msg.channel.send('bound to ' + msg.channel.name + '!');
+            msg.channel.send(`bound to <#${msg.channel.id}> !`);
+            cfg = require('../config.json');
+            const m = JSON.stringify({...cfg,default_channel: msg.channel.id}, null, 4);
+            console.log(m);
+            //thank for helping
+            fs.writeFileSync('../config.json', m, (err) => {
+                if(err) console.error('cant write file');
+            });
         } else {
             msg.channel.send('already bound!');
         }
